ffb6c33237444f01c86f138c9d6f53a93bb470a6
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2019-12-19 08:15:17 -0800

    Adding public folder

---
+++ server/public/build/precache-manifest.bf30de97c9cdcfbba9a491bf49fca714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e90e1d8731991f7075ab70de721081",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6479bcde577280c86ab5",
+    "url": "/static/css/main.c3da0036.chunk.css"
+  },
+  {
+    "revision": "d5200fee6dbd8aa777c9",
+    "url": "/static/js/2.dbfd74ce.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/static/js/2.dbfd74ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6479bcde577280c86ab5",
+    "url": "/static/js/main.871414e3.chunk.js"
+  },
+  {
+    "revision": "2550318bfc40f4f69a2f",
+    "url": "/static/js/runtime-main.fda89fee.js"
+  }
+]);
