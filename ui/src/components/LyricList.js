70e0ab31aed740aa53035b367a22a70e3eab1d54
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2019-12-18 21:25:09 -0800

    Create a way to like Lyrics

--- ui/src/components/LyricList.js
+++ ui/src/components/LyricList.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import {gql} from 'apollo-boost'
+import { useMutation } from '@apollo/react-hooks'
 
 const LyricList = ({lyrics}) => {
-    console.log(lyrics)
+    const [likeLyric] = useMutation(mutation)
     
     const printLyricList = () => {
-        return lyrics.map(({content, id}) => {
+        return lyrics.map(({content, id, likes}) => {
             return(
-                <li className="collection-item" key={id}>{content}</li>
+                <li className="collection-item" key={id}>
+                    {content}
+                    <i 
+                        className="material-icons right" 
+                        onClick={() => likeLyric({variables: {id}})}
+                    >thumb_up</i>
+                    <span className='right'>{likes}</span>
+                </li>
             )
         })
     }
@@ -17,5 +26,14 @@ const LyricList = ({lyrics}) => {
         </ul>
     )
 }
+
+const mutation = gql`
+    mutation likeLyrics($id: ID){
+        likeLyric(id: $id){
+            likes
+            id
+        }
+    }
+`
 
 export default LyricList
