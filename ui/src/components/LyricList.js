aae0b5c4bdc080b0ac5961c8f2d424c8541de20b
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2019-12-18 22:10:20 -0800

    Adding Optimistic LyricLike

--- ui/src/components/LyricList.js
+++ ui/src/components/LyricList.js
@@ -4,6 +4,20 @@ import { useMutation } from '@apollo/react-hooks'
 
 const LyricList = ({lyrics}) => {
     const [likeLyric] = useMutation(mutation)
+
+    const handleLike = (id, likes) => {
+        likeLyric({
+            variables: {id},
+            optimisticResponse: {
+                __typename: 'Mutation',
+                likeLyric: {
+                    id,
+                    __typename: 'LyricType',
+                    likes: likes + 1
+                }
+            }
+        })
+    }
     
     const printLyricList = () => {
         return lyrics.map(({content, id, likes}) => {
@@ -12,7 +26,7 @@ const LyricList = ({lyrics}) => {
                     {content}
                     <i 
                         className="material-icons right" 
-                        onClick={() => likeLyric({variables: {id}})}
+                        onClick={() => handleLike(id, likes)}
                     >thumb_up</i>
                     <span className='right'>{likes}</span>
                 </li>
